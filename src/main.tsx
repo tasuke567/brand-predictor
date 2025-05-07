0b62547a06ded0b2076f393119100b7dca300e4c
tasuke567 <80236261+tasuke567@users.noreply.github.com>
2025-05-08 02:21:27 +0700

    refactor: reorganize CSS files; move styles to dedicated index.css and update imports in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css'; // ← ✅ สำคัญมาก
+import './styles/index.css'; 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PredictionResult from './pages/PredictionResultPage.tsx'; // ✅ import หน้าใหม่ที่เราสร้างขึ้นมา
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter> {/* ✅ ห่อไว้ตรงนี้ */}
+    <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/result" element={<PredictionResult />} />
