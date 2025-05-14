8cb877604654d8f5719b6df8ee4c7d321848e471
tasuke567 <80236261+tasuke567@users.noreply.github.com>
2025-05-15 03:24:58 +0700

    refactor: remove authorization token from API headers in axios instance

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,8 @@
 // src/lib/api.ts
 import axios from "axios";
 
-const token = localStorage.getItem("token");
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "https://waka-api-hw3h.onrender.com",
   withCredentials: true,
-  // ส่ง cookie ให้ backend
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${token}`,
-  },
+
 });
