ce94dca614f70a7ad019610079d9be4820036a79
tasuke567 <80236261+tasuke567@users.noreply.github.com>
2025-05-15 03:09:06 +0700

    feat: add authorization token to API headers for authenticated requests

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,13 @@
 // src/lib/api.ts
 import axios from "axios";
 
+const token = localStorage.getItem("token");
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "https://waka-api-hw3h.onrender.com",
-  withCredentials: true,               // ส่ง cookie ให้ backend
-  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+  // ส่ง cookie ให้ backend
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  },
 });
